68c99a5ae7dc48fdbf75116672a8f827e78ca4f7
Dhruv Dave <78278281+DhruvDave12@users.noreply.github.com>
2022-01-22 19:43:42 +0530

    fixed authentication login and register

--- models/user.js
+++ models/user.js
@@ -39,30 +39,30 @@ const userSchema = new Schema({
         type: Boolean,
         required: true,
     },
-    verified: {
-        type: Boolean,
-        required: true,
-        default: false,
-    },
-    reseter: {
-        type: String,
-        required: true,
-    }
+    // verified: {
+    //     type: Boolean,
+    //     required: true,
+    //     default: false,
+    // },
+    // reseter: {
+    //     type: String,
+    //     required: true,
+    // }
 })
 
 
-userSchema.pre("save", async function (next) {
-    if(this.isModified("password")){
-        const hash = await bcrypt.hash(this.reseter, 8);
-        this.reseter = hash;
-    }
-    next();
-})
+// userSchema.pre("save", async function (next) {
+//     if(this.isModified("password")){
+//         const hash = await bcrypt.hash(this.reseter, 8);
+//         this.reseter = hash;
+//     }
+//     next();
+// })
 
-userSchema.methods.comparePassword = async function(reseter){
-    const result = await bcrypt.compareSync(reseter, this.reseter);
-    return result;
-}
+// userSchema.methods.comparePassword = async function(reseter){
+//     const result = await bcrypt.compareSync(reseter, this.reseter);
+//     return result;
+// }
 
 userSchema.plugin(passportLocalMongoose);
 module.exports = mongoose.model('User', userSchema);
--- routes/authentication.js
+++ routes/authentication.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require("../models/user.js");
 const passport = require("passport");
 const { isValidObjectId } = require("mongoose");
-const crypto = require("crypto");
+// const crypto = require("crypto");
 const multer = require("multer");
 const { storage } = require("../cloudinary/index.js");
 // here we are telling multer to store the stuff inside the storage we created in cloudinary.
@@ -20,95 +20,101 @@ router.get("/login", async (req, res) => {
 });
 
 router.post("/register", upload.single("image"), async (req, res) => {
-    
-        const {
-            email,
-            password,
-            confirmPassword,
-            username,
-            ign,
-            clgname,
-            clgid,
-            dob,
-            valorant,
-            csgo,
-            apex,
-            bgmi,
-            r6s,
-            codm,
-            codPC,
-            other,
-            isArcadian,
-        } = req.body;
 
-        if (confirmPassword !== password) {
-            req.flash("error", "Both passwords must match");
-            res.redirect("/register");
-        } else {
-            // Now we will add entry of user in our database
-            let gamesArr = [];
-            if (valorant === "on") {
-                gamesArr.push("Valorant");
-            }
-            if (csgo === "on") {
-                gamesArr.push("CSGO");
-            }
-            if (apex === "on") {
-                gamesArr.push("APEX Legends");
-            }
-            if (bgmi === "on") {
-                gamesArr.push("BGMI");
-            }
-            if (r6s === "on") {
-                gamesArr.push("Rainbow Six Siege");
-            }
-            if (codm === "on") {
-                gamesArr.push("COD Mobile");
-            }
-            if (codPC === "on") {
-                gamesArr.push("COD PC");
-            }
+    const {
+        email,
+        password,
+        confirmPassword,
+        username,
+        ign,
+        clgname,
+        clgid,
+        dob,
+        valorant,
+        csgo,
+        apex,
+        bgmi,
+        r6s,
+        codm,
+        codPC,
+        other,
+        isArcadian,
+    } = req.body;
 
+    if (confirmPassword !== password) {
+        req.flash("error", "Both passwords must match");
+        res.redirect("/register");
+    } else {
+        // Now we will add entry of user in our database
+        let gamesArr = [];
+        if (valorant === "on") {
+            gamesArr.push("Valorant");
+        }
+        if (csgo === "on") {
+            gamesArr.push("CSGO");
+        }
+        if (apex === "on") {
+            gamesArr.push("APEX Legends");
+        }
+        if (bgmi === "on") {
+            gamesArr.push("BGMI");
+        }
+        if (r6s === "on") {
+            gamesArr.push("Rainbow Six Siege");
+        }
+        if (codm === "on") {
+            gamesArr.push("COD Mobile");
+        }
+        if (codPC === "on") {
+            gamesArr.push("COD PC");
+        }
+        if(other){
             gamesArr.push(other);
-            const user = new User({
-                email: email,
-                clgName: clgname,
-                clgID: clgid,
-                games: gamesArr,
-                ign: ign,
-                dob: dob,
-                username: username,
-            });
+        }
 
-            if (isArcadian) {
-                user.isArcadian = true;
-            } else {
-                user.isArcadian = false;
+        const user = new User({
+            email: email,
+            clgName: clgname,
+            clgID: clgid,
+            games: gamesArr,
+            ign: ign,
+            dob: dob,
+            username: username,
+        });
+
+        if (isArcadian) {
+            user.isArcadian = true;
+        } else {
+            user.isArcadian = false;
+        }
+        // user.reseter = password;
+        user.profilePhoto.url = req.file.path;
+        user.profilePhoto.fileName = req.file.filename;
+
+        // removed user.register line
+
+        const registeredUser = await User.register(user,password);
+
+        req.login(registeredUser, err=>{
+            if(err){
+                res.send(err); 
             }
-            user.reseter = password;
-            user.profilePhoto.url = req.file.path;
-            user.profilePhoto.fileName = req.file.filename;
-            req.flash("success", "Your Account Has Been Registered, Please login!");
-            res.redirect("/login");
+            req.flash("success", `Welcome, ${user.username}`);
+            res.redirect(`/homepage/${user._id}`);
+        })
         
-    } 
+    }
 });
 
-router.post(
-    "/login",
-    passport.authenticate("local", {
-        failureFlash: true,
-        failureRedirect: "/login",
-    }),
-    async (req, res) => {
-        const { email } = req.body;
-        req.flash("success", "Welcome Back");
+router.post("/login", passport.authenticate("local", { failureFlash: true, failureRedirect: "/login", }), async (req, res) => {
+    const { email } = req.body;
+    req.flash("success", "Welcome Back");
 
-        const user = await User.findOne({ email: email });
-        const redirectUrl = req.session.returnTo || `/homepage/${user._id}`;
-        delete req.session.returnTo;
-        res.redirect(redirectUrl);
-    }
+    const user = await User.findOne({ email: email });
+    const redirectUrl = req.session.returnTo || `/homepage/${user._id}`;
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+}
 );
 
 router.get("/logout", async (req, res) => {
